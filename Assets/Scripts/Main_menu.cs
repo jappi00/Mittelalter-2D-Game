6a43e5d1fbd5d091dd2b76b78fbf95a2fbb494b0
jappi00 <jappi2000@ewetel.net>
2015-12-24 14:02:44 +0100

    edit menu
    add functions to buttons

---
+++ Assets/Scripts/Main_menu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+   
+
+public class Main_menu : MonoBehaviour {
+    
+    public void ExitGame() {
+       Application.Quit();
+       Debug.Log ("EXIT");
+    }
+    public void ChangeScene (int sceneChangeTo){
+        Application.LoadLevel(sceneChangeTo);
+        Debug.Log("change scene");
+    }
+}
