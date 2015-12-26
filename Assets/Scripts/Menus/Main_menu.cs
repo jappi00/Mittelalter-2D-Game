56379d0850d3c3ee19819377340be9c4f9d0309d
jappi00 <jappi2000@ewetel.net>
2015-12-26 14:19:14 +0100

    add Placeholder.txt

--- Assets/Scripts/Menus/Main_menu.cs
+++ Assets/Scripts/Menus/Main_menu.cs
@@ -18,8 +18,9 @@ public class Main_menu : MonoBehaviour {
         Debug.Log("change scene");
     }
 
-    public void ChangePanel(GameObject Objekt )
+    public void ChangePanel(GameObject Objekt)
     {
+        bool Active_Inactive = Objekt.activeSelf;
         Objekt.SetActive(true);
     }
 }
