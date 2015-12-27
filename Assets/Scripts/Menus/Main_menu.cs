a6ccd00fac2f995de5d84ccd81d82585ee690501
jappi00 <jappi2000@ewetel.net>
2015-12-27 21:45:12 +0100

    add music control

--- Assets/Scripts/Menus/Main_menu.cs
+++ Assets/Scripts/Menus/Main_menu.cs
@@ -5,22 +5,28 @@ using UnityEngine.UI;
 
 public class Main_menu : MonoBehaviour {
     
+    //Spiel beenden
     public void ExitGame() {
        Application.Quit();
        Debug.Log ("EXIT");
     }
+
+    //Andere Scene aufrufen
     public void ChangeScene (int sceneChangeTo){
         Application.LoadLevel(sceneChangeTo);
         Debug.Log("change scene");
     }
+    //Anderes Menü öffnen
     public void menuChangeTo(GameObject objektToChange){
+        //wechsel zu aus
         if (objektToChange.activeSelf)
         {
-            objektToChange.active = false;
+            objektToChange.SetActive(false);
         }
+        //wechsel zu an
         else
         {
-            objektToChange.active = true;
+            objektToChange.SetActive(true);
         }
     }
 }
---
+++ Assets/Scripts/Menus/MusicManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+    //Referenzen
+    public Slider slider;//Musik Slider
+    public Toggle toggle;
+    public int setToValue;
+
+    public void OnOfMusic(bool ToogleInput)
+    {
+        if (ToogleInput)
+        {
+            slider.value = setToValue;
+        }
+        else
+        {
+            slider.value = 0;
+        }
+        Debug.Log(slider.value);
+    }
+   
+    public void SliderInput(float Value)
+    {
+        Debug.Log(Value);
+        if(Value == 0)
+        {
+            toggle.isOn = false;
+        }
+        else
+        {
+            toggle.isOn = true;
+        }
+    }
+}
