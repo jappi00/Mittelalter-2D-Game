a48d1d235edf663d0cb378f5ae396abe4dd1dce5
jappi00 <jappi2000@ewetel.net>
2015-12-30 22:01:10 +0100

    Add comments too Live.cd

--- Assets/Scripts/Player/Live.cs
+++ Assets/Scripts/Player/Live.cs
@@ -11,19 +11,21 @@ public class Live : MonoBehaviour {
     public int minLive; // Minimales Leben Standard = 0
     public int maxLive; // Maximales Leben Standard = 100
 
-	// Use this for initialization
+	// Überprüfen von den eingegebenen Daten
 	void Start () {
-        if (minLive >= maxLive)
+        if (minLive >= maxLive) //wenn das maximale Leben kleiner oder gleich dem minimalem Leben ist wird wie unten geschrieben die Werte gesetzt
         {
             Debug.LogError("Das minimale Leben ist grösser oder gleich zum maximalen Leben, das Maximale wird zu 100 und das minimale zu 0 gesetzt");
             minLive = 0;
             maxLive = 100;
             
         }
-        if (minLive == currentLife)
+        if (minLive >= currentLife)//wenn das minimale Leben größer oder Gleich dem jetzigen Leben ist wird ein Fehler ausgegeben
         {
             Debug.LogError("Das momentane Leben ist gleich dem minimalen Leben also ist man sofort tot!");
         }
+         
+        //Einrichten des Lebensbalken
         healthbar.minValue = minLive;
         healthbar.maxValue = maxLive;
         healthbar.value = currentLife;
