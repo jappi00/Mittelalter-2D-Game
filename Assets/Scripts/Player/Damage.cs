533ff61a008c04ca1c65fe3b330191e7549939c4
jappi00 <jappi2000@ewetel.net>
2015-12-30 22:10:34 +0100

    Update Damage.cs

--- Assets/Scripts/Player/Damage.cs
+++ Assets/Scripts/Player/Damage.cs
@@ -4,17 +4,11 @@ using UnityEngine.UI;
 
 public class Damage : MonoBehaviour {
 
-    private Transform player;
-
     private float currentHealth;
 
     public Slider healthbar; //Lebens anzeige
     public float damage;
 
-    void Start()
-    {
-        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-    }
     // Update is called once per frame
     void OnTriggerEnter2D(Collider2D col)
     {
